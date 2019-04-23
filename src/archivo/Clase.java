3f2a93d2ad2996f9ea325582ff38652f99016737
GDelga <gdelga02@ucm.es>
2019-04-23 14:45:23 +0200

    clases basicas

---
+++ src/archivo/Clase.java
@@ -0,0 +1,56 @@
+package archivo;
+
+import java.util.ArrayList;
+
+/**
+ * @author Guillermo Delgado Yepes
+ */
+public class Clase {
+
+	private String nombre;
+	private double[][] matriz;
+	private ArrayList<Double> centro;
+
+	
+	public Clase(ArrayList<Elemento> elementos, String nombre, ArrayList<Double> ds){		
+		this.nombre = nombre;
+		this.centro=ds;
+		matriz = new double[elementos.size()][elementos.get(0).getLista().size()];
+		int i=0;
+			for(Elemento elem : elementos) {
+				int k=0;
+				for(Double num : elem.getLista()) {
+					matriz[i][k]=num;
+					++k;
+				}
+				++i;
+			}
+	}
+	
+	
+	public ArrayList<Double> getCentro(){
+		return centro;
+	}
+	
+	public void setCentro(ArrayList<Double> centro) {
+		
+		this.centro= centro;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public double[][] getMatriz(){
+		return matriz;
+	}
+	
+	public void setMatriz(double[][] matriz) {
+		this.matriz = matriz;
+	}
+
+}
---
+++ src/archivo/Elemento.java
@@ -0,0 +1,35 @@
+package archivo;
+
+import java.util.ArrayList;
+
+/**
+ * @author Guillermo Delgado Yepes
+ */
+public class Elemento {
+
+	private ArrayList<Double> lista;
+	public String nombre;
+
+	
+	public Elemento(ArrayList<Double> lista, String nombre) {
+		super();
+		this.lista=lista;
+		this.nombre = nombre;
+	}
+		
+	public ArrayList<Double> getLista() {
+		return lista;
+	}
+	
+	public void setLista(ArrayList<Double> lista) {
+		this.lista = lista;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+}
